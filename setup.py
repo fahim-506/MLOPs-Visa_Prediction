06cf80bfc9a5063bb0323f76495d86f129d0ea24
fahim-506 <fahimp506@gmail.com>
2026-02-20 15:20:43 +0530

    Setuptools added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    auther="Fahim",
+    author_email="fahimcs06@gmail.com",
+    packages=find_packages()
+)
