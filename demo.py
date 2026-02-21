aaa746347f4daaa53866418bd2e29b29cc478a84
fahim-506 <fahimp506@gmail.com>
2026-02-21 15:07:15 +0530

    try exception block added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from us_visa.logger import logging
+from us_visa.exception import UsVisaException
+import sys
 
 logging.info("Welcome To Our Custom Log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise UsVisaException(e,sys)
