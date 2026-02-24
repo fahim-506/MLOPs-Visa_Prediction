8e79b2c256d86f776d047749e039ec249b7fca2d
fahim-506 <fahimp506@gmail.com>
2026-02-24 12:39:45 +0530

    run pipeline

--- demo.py
+++ demo.py
@@ -1,10 +1,24 @@
-from us_visa.logger import logging
-from us_visa.exception import UsVisaException
-import sys
+# # from us_visa.logger import logging
+# # from us_visa.exception import UsVisaException
+# # import sys
 
-logging.info("Welcome To Our Custom Log")
+# # logging.info("Welcome To Our Custom Log")
 
-try:
-    a = 2/0
-except Exception as e:
-    raise UsVisaException(e,sys)
+# # try:
+# #     a = 2/0
+# # except Exception as e:
+# #     raise UsVisaException(e,sys)
+
+# # changes
+# import os
+# from us_visa.constants import *
+# mongo_db_url = MONGODB_URL_KEY
+# print(mongo_db_url)
+# # changes after pipeline train
+from dotenv import load_dotenv
+load_dotenv()
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
